d5e0f6e3116a049876c9830e07aa449a0951b7be
robsonr4 <falkowski.robert4@gmail.com>
2021-03-12 22:53:43 +0100

    Chuj ci w dupę

--- hangman.py
+++ hangman.py
@@ -10,38 +10,36 @@ zodiac = ["cancer", "taurus", "capricorn", "libra", "pisces", "leo", "escorpio",
 ran_zodiac = random.choice(zodiac)
 attempts = 2 * len(ran_zodiac)
 print("Here u have some hints:\n")
-final = " _ " * len(ran_zodiac)
+final = list('_' * len(ran_zodiac))
 print(f"The first letter of a zodiac is {ran_zodiac[0]}")
 letters = []
 words = []
 guessed = False
-print(final)
-print("\n")
 
 while attempts > 0 and not guessed:
+    print(" ".join(final))
+    print("\n")
     guess = input("Write a word or a letter: ").lower()
-    print("")
+    print()
     if len(guess) == 1 and guess.isalpha():
         if guess in letters:
             print(f"{guess} was already used.")
         elif guess not in ran_zodiac:
-            print(f"Try again. No {guess} in the  sign.")
+            print(f"Try again. No {guess} in the sign.")
             attempts -= 1
             letters.append(guess)
         else:
             print(f"Muy bien! {guess} is in the sign" )
-            letters.append(guess)
-            separated = list(final)
+            letters.append(guess) 
             numer = [i for i, letter in enumerate(ran_zodiac) if letter == guess]
             for index in numer:
-                separated[index] = guess
-            final = "".join(separated)
+                final[index] = guess
             if "_" not in final:
                 guessed = True
-
-    elif len(guess) == len(ran_zodiac) and guess.isalpha():
-        if guess in words:
+    elif guess.isalpha():
+        if guess in ran_zodiac:
             print("Tranquilo! You already know your match.")
+            guessed = True
         elif guess != ran_zodiac:
             print("Hold your horses! Guess is incorrect.")
             attempts -= 1
@@ -49,8 +47,6 @@ while attempts > 0 and not guessed:
         else:
             guessed = True
             final = ran_zodiac
-    
-
     else:
         print("""Not this time. Go buy a cat""")
 
